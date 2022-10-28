8b34317a918662ac111287161bf97bd9fb50b7d8
Ville Koskela <ville.koskela@terveystalo.com>
2022-10-28 14:52:07 +0300

    minor tweaks

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ const run = async (): Promise<void> => {
   const bumpType = checkBumpType(repoInfo.headMessage);
   const username = getInput('username');
   info(`username: ${username}`)
-  await exec('git config', ['--global', 'user.name', 'ville-koskela']);
+  await exec('git config', ['--global', 'user.name', 'Ville Koskela']);
   await exec('git config', ['--global', 'user.email', 'noreply@example.com']);
   info(`bumping with ${bumpType}`)
-  await exec('npm version', [bumpType, '--force']);
+  await exec('npm version', [bumpType]);
   await exec('git push');
-  await exec('git push', ['--tags']);
+  await exec('git push', ['--follow-tags']);
   setOutput('success', true);
 };
 

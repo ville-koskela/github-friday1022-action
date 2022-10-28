2b9095a1369f6955a60d8f7d15e22c0c8b44dcdd
Ville Koskela <ville.koskela@terveystalo.com>
2022-10-28 11:33:00 +0000

    build version 1.1.0

---
+++ dist/types/index.d.ts
@@ -0,0 +1,4 @@
+export declare type BumpType = "major" | "minor" | "patch";
+export interface RepositoryInfo {
+    headMessage: string;
+}
---
+++ dist/utils/checkBumpType.d.ts
@@ -0,0 +1,3 @@
+import { BumpType, RepositoryInfo } from '../types';
+export declare const getRepositoryInfo: () => RepositoryInfo;
+export declare const checkBumpType: (message: string) => BumpType;

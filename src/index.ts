45031107ad27e7b8741192d32a857c5dd44a1264
Ville Koskela <ville.koskela@terveystalo.com>
2022-10-28 14:35:04 +0300

    fix git config

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,11 @@ const run = async (): Promise<void> => {
   const repoInfo = getRepositoryInfo();
   const bumpType = checkBumpType(repoInfo.headMessage);
   const username = getInput('username');
-  const email = getInput('email');
+  // const email = getInput('email');
+  console.log('username: ', username)
   await exec('npm version', [bumpType]);
-  await exec('git config', ['--global', 'user.name', username]);
-  await exec('git config', ['--global', 'user.email', email]);
+  await exec('git config', ['--global', 'user.name', 'ville-koskela']);
+  await exec('git config', ['--global', 'user.email', 'noreply@example.com']);
   await exec('git push');
   await exec('git push', ['--follow-tags']);
   setOutput('success', true);

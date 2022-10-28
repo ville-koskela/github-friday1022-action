14c8f820d3d60231ec57749cea9c6e23aefb3f45
Ville Koskela <ville.koskela@terveystalo.com>
2022-10-28 12:29:05 +0300

    study passing values from job to another

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import { getInput, setOutput } from '@actions/core';
 const bumpType = getInput('bump-type');
 console.log('bumpType', bumpType)
 console.log('--- new version published ---')
+setOutput('version', '1.0.0')
 setOutput('success', true)

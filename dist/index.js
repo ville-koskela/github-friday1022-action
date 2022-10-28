0a51f97fc3eeb451226858cbf98631262f7553fd
Ville Koskela <ville.koskela@terveystalo.com>
2022-10-28 10:42:25 +0000

    build version

--- dist/index.js
+++ dist/index.js
@@ -2826,6 +2826,7 @@ const bumpType = (0, core_1.getInput)('bump-type');
 console.log('bumpType', bumpType);
 (0, core_1.setOutput)('version', '1.1.0');
 (0, core_1.setOutput)('success', true);
+console.log('done!');
 
 })();
 

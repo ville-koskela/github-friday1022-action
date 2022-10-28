c45b2ff70c56ab9b4b76be50b71e7f2c66a12712
Ville Koskela <ville.koskela@terveystalo.com>
2022-10-28 12:12:43 +0300

    added build job

---
+++ README.md
@@ -0,0 +1,10 @@
+# My Friday Action
+
+## development
+
+Setup development environment by installing dependencies: `npm install` 
+
+## build
+
+Build new version of the action: `npm run build`
+
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import { getInput, setOutput } from '@actions/core';
 
 const bumpType = getInput('bump-type');
 console.log('bumpType', bumpType)
+console.log('--- new version published ---')
 setOutput('success', true)
